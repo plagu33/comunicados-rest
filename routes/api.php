8c37363e2156f5d940e71b7233620b65ab0ca9e2
Manuel Meriño <manuel.merino.c@gmail.com>
2018-11-05 18:40:45 -0300

    update

--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,26 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// get list of tasks
+Route::get('tasks','TaskController@index');
+// get specific task
+Route::get('task/{id}','TaskController@show');
+// create new task
+Route::post('task','TaskController@store');
+// update existing task
+Route::put('task','TaskController@store');
+// delete a task
+Route::delete('task/{id}','TaskController@destroy');
+
+*/
+
+// get list of tasks
+Route::get('demos','DemoController@index');
+
+// create new demo
+Route::post('demo','DemoController@store');
