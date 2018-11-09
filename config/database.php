06c385bee47347509214a65190db44fda283228c
Manuel Meriño <manuel.merino.c@gmail.com>
2018-11-09 16:57:36 -0300

    add table MT_CLIENT

--- config/database.php
+++ config/database.php
@@ -33,16 +33,9 @@ return [
 
     'connections' => [
 
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
-        ],
-
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HDB_CONNECTIONOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
@@ -52,22 +45,40 @@ return [
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
-            'strict' => true,
+            'strict' => false,
             'engine' => null,
         ],
 
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
+        'umas' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST_UMAS', '127.0.0.1'),
+            'port' => env('DB_PORT_UMAS', '3306'),
+            'database' => env('DB_DATABASE_UMAS', 'forge'),
+            'username' => env('DB_USERNAME_UMAS', 'forge'),
+            'password' => env('DB_PASSWORD_UMAS', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
-            'schema' => 'public',
-            'sslmode' => 'prefer',
+            'strict' => false,
+            'engine' => null,
+        ],
+
+        'aula' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST_AULA', '127.0.0.1'),
+            'port' => env('DB_PORT_AULA', '3306'),
+            'database' => env('DB_DATABASE_AULA', 'forge'),
+            'username' => env('DB_USERNAME_AULA', 'forge'),
+            'password' => env('DB_PASSWORD_AULA', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => false,
+            'engine' => null,
         ],
 
         'sqlsrv' => [
