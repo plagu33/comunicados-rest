b3e75bbe5a42f787fd164d192784b36e2e51b359
jota <jotach22@gmail.com>
2018-11-19 20:33:40 -0300

    Update 2018_11_19_225908_create_ra_act_eval_seccion_table.php
    update

--- database/migrations/comunicados_umas/2018_11_19_225908_create_ra_act_eval_seccion_table.php
+++ database/migrations/comunicados_umas/2018_11_19_225908_create_ra_act_eval_seccion_table.php
@@ -14,8 +14,22 @@ class CreateRaActEvalSeccionTable extends Migration
     public function up()
     {
         Schema::connection('umas')->create('ra_ActEval_Seccion', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
+            $table->decimal('Ano',4,0)->nullable();
+            $table->decimal('Periodo',3,0)->nullable();
+            $table->string('CodSede',30)->nullable();
+            $table->string('Codcarr',30)->nullable();
+            $table->string('CodRamo',20)->nullable();
+            $table->decimal('CodSecc',3,0)->nullable();
+            $table->string('Actividad',30)->nullable();
+            $table->smallInteger('Cantidad')->nullable();
+            $table->string('Elimina_mas_baja',2)->nullable();
+            $table->string('Posee_recuperativa',2)->nullable();
+            $table->integer('Porcentaje')->nullable();
+            $table->dateTime('Fec_Ini_IngNotas')->nullable();
+            $table->dateTime('Fec_Fin_IngNotas')->nullable();
+            $table->string('Usa_mas_Baja',2)->nullable();
+            $table->string('Usa_Recuperativa',2)->nullable();
+           
         });
     }
 
