e003a702dc99a814b7a3c74bc1b900987913e230
Manuel Meriño <manuel.merino.c@gmail.com>
2018-11-19 20:21:59 -0300

    update
    se agregan tablas

---
+++ database/migrations/comunicados_umas/2018_11_19_231501_create_ra_acteval_nota_det_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRaActevalNotaDetTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('umas')->create('ra_Acteval_Nota_det', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection('umas')->dropIfExists('ra_Acteval_Nota_det');
+    }
+}
