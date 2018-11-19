5119f2d3662a0306e382406ff31adbe7c5fa9528
jota <jotach22@gmail.com>
2018-11-19 20:52:31 -0300

    Update 2018_11_19_230842_create_ra_acteval_seccion_det_table.php
    update jose

--- database/migrations/comunicados_umas/2018_11_19_230842_create_ra_acteval_seccion_det_table.php
+++ database/migrations/comunicados_umas/2018_11_19_230842_create_ra_acteval_seccion_det_table.php
@@ -14,8 +14,19 @@ class CreateRaActevalSeccionDetTable extends Migration
     public function up()
     {
         Schema::connection('umas')->create('ra_Acteval_Seccion_det', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
+            $table->decimal('Ano',4,0)->nullable();
+            $table->decimal('Periodo',3,0)->nullable();
+            $table->string('CodSede',30)->nullable();
+            $table->string('Codcarr',30)->nullable();
+            $table->string('CodRamo',20)->nullable();
+            $table->decimal('CodSecc',3,0)->nullable();
+            $table->string('actividad',30)->nullable();
+            $table->smallInteger('Linea')->nullable();
+            $table->integer('Porcentaje')->nullable();
+            $table->dateTime('FecIngInicial')->nullable();
+            $table->dateTime('FecIngFinal')->nullable();
+
+
         });
     }
 
