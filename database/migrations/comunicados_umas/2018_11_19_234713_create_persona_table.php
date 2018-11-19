ab27cf86d2d9dca7c1858b02486e8efdf2406979
Manuel Meriño <manuel.merino.c@gmail.com>
2018-11-19 20:50:32 -0300

    update

---
+++ database/migrations/comunicados_umas/2018_11_19_234713_create_persona_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePersonaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('umas')->create('persona', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection('umas')->dropIfExists('persona');
+    }
+}
