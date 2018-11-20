c3618ff9cc1b3473becabf1fd6e579647d322897
Jose Escalona <jose.escalona@tsoftlatam.com>
2018-11-20 00:25:03 -0300

    Update 2018_11_19_232316_create_RA_RESERVASALA_table.php
    Update tabla RA_RESERVASALA

--- database/migrations/comunicados_umas/2018_11_19_232316_create_RA_RESERVASALA_table.php
+++ database/migrations/comunicados_umas/2018_11_19_232316_create_RA_RESERVASALA_table.php
@@ -14,9 +14,23 @@ class CreateRARESERVASALATable extends Migration
     public function up()
     {
         Schema::connection('umas')->create('RA_RESERVASALA', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
-        });
+            $table->string('CODSAL',12)->nullable();
+            $table->string('CODPROF',30)->nullable();
+            $table->string('CODMOD',3)->nullable();
+            $table->string('DIA',9)->nullable();
+            $table->string('CODRAMO',20)->nullable();
+            $table->decimal('CODSECC',3,0)->nullable();
+            $table->decimal('ANO',4,0)->nullable();
+            $table->decimal('PERIODO',3,0)->nullable();
+            $table->integer('REGIMEN')->nullable();
+            $table->dateTime('FECHA')->nullable();
+            $table->string('TIPO',30)->nullable();
+            $table->string('CODSEDE',30)->nullable();
+            $table->string('Glosa',500)->nullable();
+            $table->string('CODCARR',30)->nullable();
+            $table->string('ESTADO',1)->nullable();
+            $table->string('marcado',1)->nullable();
+         });
     }
 
     /**
