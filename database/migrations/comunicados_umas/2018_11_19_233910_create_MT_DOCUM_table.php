1e919706c13ff5eded91ddf6c49d798adc207382
SuperSayayin <jotach22@gmail.com>
2018-11-21 12:36:28 -0300

    Update 2018_11_19_233910_create_MT_DOCUM_table.php
    Update tabla MT_DOCUM

--- database/migrations/comunicados_umas/2018_11_19_233910_create_MT_DOCUM_table.php
+++ database/migrations/comunicados_umas/2018_11_19_233910_create_MT_DOCUM_table.php
@@ -14,9 +14,28 @@ class CreateMTDOCUMTable extends Migration
     public function up()
     {
         Schema::connection('umas')->create('MT_DOCUM', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
-        });
+            $table->decimal('TIPODOC',4,0)->nullable();
+            $table->string('NOMBRE',60)->nullable();
+            $table->string('VENDIBLE',1)->nullable();
+            $table->string('USUARIO',8)->nullable();
+            $table->dateTime('FECMOD')->nullable();
+            $table->string('DEFECTO',1)->nullable();
+            $table->string('ABREVIATURA',10)->nullable();
+            $table->string('CUENTA',20)->nullable();
+            $table->integer('ANALISIS')->nullable();
+            $table->string('PROTESTO',1)->nullable();
+            $table->string('PorCaja',1)->nullable();
+            $table->integer('Flujo')->nullable();
+            $table->decimal('TIPDOCPROTESTO',10,0)->nullable();
+            $table->string('DOCRESPALDO',1)->nullable();
+            $table->string('PagoWeb',2)->nullable();
+            $table->integer('CODIGO_ERP')->nullable();
+            $table->string('protestable',1)->nullable();
+            $table->integer('CODIGO_SII')->nullable();
+            $table->string('VentaReversa',2)->nullable();
+            $table->string('VENTACOMPLEMENTARIA',2)->nullable();
+            $table->char('GENERAINT',2)->nullable();
+            $table->decimal('MONEDA_AD',1,0)->nullable();
     }
 
     /**
